afc5cb69bea2fca76a800e796e35d087c35eea93
Aynur Rahman Talukdar <128217234+dareDEVil-aynur@users.noreply.github.com>
2024-01-19 15:50:49 +0200

    fix println

--- round2/mean/src/main/java/fi/tuni/prog3/Mean.java
+++ round2/mean/src/main/java/fi/tuni/prog3/Mean.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Mean {
     public static void main(String[] args) {
         try (Scanner scanner = new Scanner(System.in)) {
-            System.out.print("Enter numbers: ");
+            System.out.println("Enter numbers: ");
             String line = scanner.nextLine();
             String[] numbers = line.split(" ");
             double sum = 0;
