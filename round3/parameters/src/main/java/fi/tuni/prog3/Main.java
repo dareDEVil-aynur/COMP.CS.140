de5eca3659e14e2f34cc11ad1962da6ca6d2676f
Aynur Rahman Talukdar <128217234+dareDEVil-aynur@users.noreply.github.com>
2024-01-26 13:24:35 +0200

    fix layout

--- round3/parameters/src/main/java/fi/tuni/prog3/Main.java
+++ round3/parameters/src/main/java/fi/tuni/prog3/Main.java
@@ -8,35 +8,45 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Parameters:");
-
         List<String> parameters = new ArrayList<>();
-        String input;
-        while (!(input = scanner.nextLine()).isEmpty()) {
+
+        System.out.println("Parameters:");
+        while (scanner.hasNextLine()) {
+            String input = scanner.nextLine();
+            if (input.isEmpty()) {
+                scanner.close();
+                break;
+            }
             parameters.add(input);
         }
 
         Collections.sort(parameters);
 
-        int maxNumberLength = String.valueOf(parameters.size()).length();
-        int maxParamLength = parameters.stream().map(String::length).max(Integer::compare).orElse(0);
+        int col1Width = String.valueOf(parameters.size()).length() + 2;
+        int col2Width = parameters.stream().mapToInt(String::length).max().orElse(0) + 2;
 
-        String rowFormat = "# %" + maxNumberLength + "d | %-"
-                + maxParamLength + "s #\n";
-
-        String border = String.format("#%s#", "-".repeat(maxNumberLength + maxParamLength + 3));
-
-        System.out.println(border.replace("-", "#"));
+        createBorder(col1Width, col2Width, "top");
 
         for (int i = 0; i < parameters.size(); i++) {
-            if (i > 0) {
-                System.out.println(border.replace("#", "+"));
+            System.out.format("# %" + (col1Width - 2) + "d | %-" + (col2Width - 2) + "s #\n", i + 1, parameters.get(i));
+            if(i != parameters.size() - 1) {
+                createBorder(col1Width, col2Width, "middle");
             }
-            System.out.printf(rowFormat, i + 1, parameters.get(i));
         }
 
-        System.out.println(border.replace("-", "#"));
+        createBorder(col1Width, col2Width, "bottom");
+    }
 
-        scanner.close();
+    private static void createBorder(int col1Width, int col2Width, String type) {
+        System.out.print("#");
+        char midChar = type.equals("middle") ? '-' : '#';
+        for (int i = 0; i < col1Width; i++) {
+            System.out.print(midChar);
+        }
+        System.out.print(type.equals("middle") ? "+" : "#");
+        for (int i = 0; i < col2Width; i++) {
+            System.out.print(midChar);
+        }
+        System.out.println("#");
     }
 }
