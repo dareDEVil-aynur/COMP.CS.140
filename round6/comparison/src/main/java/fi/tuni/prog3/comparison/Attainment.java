7777bf38142b075f7d24d4fe807b7b1593965d42
Aynur Rahman Talukdar <128217234+dareDEVil-aynur@users.noreply.github.com>
2024-02-16 19:48:32 +0200

    comparison

---
+++ round6/comparison/src/main/java/fi/tuni/prog3/comparison/Attainment.java
@@ -0,0 +1,43 @@
+package fi.tuni.prog3.comparison;
+import java.util.Comparator;
+
+public class Attainment implements Comparable<Attainment>{
+    private final String courseCode;
+    private final String studentNumber;
+    private final int grade;
+    public static final Comparator<Attainment> CODE_STUDENT_CMP = Comparator.comparing(Attainment::getCourseCode).thenComparing(Attainment::getStudentNumber);
+    public static final Comparator<Attainment> CODE_GRADE_CMP = Comparator.comparing(Attainment::getCourseCode).thenComparing(Attainment::getGrade, Comparator.reverseOrder());
+
+    public Attainment(String courseCode, String studentNumber, int grade) {
+        this.courseCode = courseCode;
+        this.studentNumber = studentNumber;
+        this.grade = grade;
+    }
+
+    public String getCourseCode() {
+        return courseCode;
+    }
+
+    public String getStudentNumber() {
+        return studentNumber;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+    
+    @Override
+    public String toString(){
+        return String.format("%s %s %d", courseCode, studentNumber, grade);
+    }
+    
+    @Override
+    public int compareTo(Attainment other){
+        int cmp = studentNumber.compareTo(other.studentNumber);
+        if(cmp == 0){
+            cmp = courseCode.compareTo(other.courseCode);
+        }
+        return cmp;
+    }  
+}
+   
